7d5365ef606b0e0a876a0fbcf033d13ef8eda4fb
김선재 <gimseonjae@gimseonjaeui-MacBookPro.local>
2020-12-22 17:38:43 +0900

    modifiedupdate react

--- mayweather/src/App.js
+++ mayweather/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to MayWeather Service</h1>
+      <h2>restart merge</h2>
     </div>
   );
 }
