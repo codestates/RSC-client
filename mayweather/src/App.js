bed95d296e397b9b8c24cbef823cf5a255ccb2ea
jyshimmy <66928595+jyshimmy@users.noreply.github.com>
2020-12-22 17:57:13 +0900

    testing pull upstream

--- mayweather/src/App.js
+++ mayweather/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Welcome to MayWeather Service</h1>
       <h2>restart merge</h2>
+      <div>git pull upstream dev not working</div>
     </div>
   );
 }
