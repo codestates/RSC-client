96831be9a75a47763345e115cc836b744e1f11e3
김선재 <gimseonjae@gimseonjaeui-MacBookPro.local>
2021-01-04 16:12:57 +0900

    change_background_css

--- mayweather/src/pages/Login.js
+++ mayweather/src/pages/Login.js
@@ -13,7 +13,7 @@ class Login extends React.Component {
       errorMessage: "",
       errorMessage_0: "",
       visitorLocation: "",
-      isMember: ""
+      isMember: "",
     };
     this.handleInputValue = this.handleInputValue.bind(this);
     this.handleLogin_0 = this.handleLogin_0.bind(this);
@@ -74,7 +74,7 @@ class Login extends React.Component {
           console.log("post login res >>>", res);
           this.setState({
             isMember: false,
-          })
+          });
           this.props.handleisMember(this.state.isMember);
           handleResponseSuccess();
         });
@@ -83,88 +83,90 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div>
-        <nav className="login_navbar">
-          <ul>
-            <li>MayWeather24</li>
-            <li>
-              <Link to="./" className="login_tohome">
-                Home
-              </Link>
-            </li>
-          </ul>
-        </nav>
+      <div className="login">
         <div>
-          <center>
-            <h1>Login Page</h1>
-          </center>
-        </div>
-        <div className="loginpage">
-          <div className="login_member">
-            <h3>회원 로그인</h3>
-            <form onSubmit={(e) => e.preventDefault()}>
-              <div className="idbox">
-                <span>회원 ID</span>&nbsp;
-                <input
-                  type="text"
-                  placeholder="ID를 입력하세요"
-                  onChange={this.handleInputValue("userId")}
-                ></input>
-              </div>
-              <div className="pwbox">
-                <span>비밀번호</span>&nbsp;
-                <input
-                  type="password"
-                  placeholder="비밀번호를 입력하세요"
-                  onChange={this.handleInputValue("password")}
-                ></input>
-              </div>
-              <br />
-              <button
-                className="loginbtn"
-                type="submit"
-                onClick={this.handleLogin}
-              >
-                로그인
-              </button>
-              {<div>{this.state.errorMessage}</div>}
-            </form>
-            <br />
-            <div>
-              <Link to="./signup">아직 회원이 아니신가요?</Link>
-              <br />
-              {/* <Link to="/">비회원으로 로그인하기</Link> */}
-            </div>
+          <nav className="login_navbar">
+            <ul>
+              <li>MayWeather24</li>
+              <li>
+                <Link to="./" className="login_tohome">
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div>
+            <center>
+              <h1>Login Page</h1>
+            </center>
           </div>
-          <div className="login_nonmember">
-            <h3>비회원 로그인</h3>
-            <br />
-            <div>
-              보고싶은 도시&nbsp;
-              <select onChange={this.handleInputValue("visitorLocation")}>
-                <option value="">도시선택</option>
-                <option value="seoul">서울</option>
-                <option value="incheon">인천</option>
-                <option value="daegu">대구</option>
-                <option value="gwangju">광주</option>
-                <option value="busan">부산</option>
-              </select>
+          <div className="loginpage">
+            <div className="login_member">
+              <h3>회원 로그인</h3>
+              <form onSubmit={(e) => e.preventDefault()}>
+                <div className="idbox">
+                  <span>회원 ID</span>&nbsp;
+                  <input
+                    type="text"
+                    placeholder="ID를 입력하세요"
+                    onChange={this.handleInputValue("userId")}
+                  ></input>
+                </div>
+                <div className="pwbox">
+                  <span>비밀번호</span>&nbsp;
+                  <input
+                    type="password"
+                    placeholder="비밀번호를 입력하세요"
+                    onChange={this.handleInputValue("password")}
+                  ></input>
+                </div>
+                <br />
+                <button
+                  className="loginbtn"
+                  type="submit"
+                  onClick={this.handleLogin}
+                >
+                  로그인
+                </button>
+                {<div>{this.state.errorMessage}</div>}
+              </form>
               <br />
+              <div>
+                <Link to="./signup">아직 회원이 아니신가요?</Link>
+                <br />
+                {/* <Link to="/">비회원으로 로그인하기</Link> */}
+              </div>
+            </div>
+            <div className="login_nonmember">
+              <h3>비회원 로그인</h3>
               <br />
-              <br />
-              {/* <Link to="./content"> */}
-              {/* <button
+              <div>
+                보고싶은 도시&nbsp;
+                <select onChange={this.handleInputValue("visitorLocation")}>
+                  <option value="">도시선택</option>
+                  <option value="seoul">서울</option>
+                  <option value="incheon">인천</option>
+                  <option value="daegu">대구</option>
+                  <option value="gwangju">광주</option>
+                  <option value="busan">부산</option>
+                </select>
+                <br />
+                <br />
+                <br />
+                {/* <Link to="./content"> */}
+                {/* <button
                   onClick={this.props.handleVisitorLocation.bind(
                     this.state.visitorLocation
                   )}
                 >
                   로그인
                 </button> */}
-              <button type="submit" onClick={this.handleLogin_0}>
-                로그인
-              </button>
-              {<div>{this.state.errorMessage_0}</div>}
-              {/* </Link> */}
+                <button type="submit" onClick={this.handleLogin_0}>
+                  로그인
+                </button>
+                {<div>{this.state.errorMessage_0}</div>}
+                {/* </Link> */}
+              </div>
             </div>
           </div>
         </div>
